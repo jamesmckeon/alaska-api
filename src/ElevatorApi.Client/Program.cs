ccb4b4bda71f78b593b572ef2e67d740418131e3
jamesmckeon <jbmckeon@comcast.net>
2025-12-17 11:19:35 -0800

    found bug, logged as GH issue #5

--- src/ElevatorApi.Client/Program.cs
+++ src/ElevatorApi.Client/Program.cs
@@ -1,8 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net.Security;
+﻿// displays message and prompts user to exit the program
 
 // ReSharper disable All
+
+using System.Net.Http.Json;
+using System.Text.Json;
+using ElevatorApi.Api.Models;
 
 void Exit(string message)
 {
@@ -14,21 +16,110 @@ void Exit(string message)
 
 try
 {
+    #region Setup
+
     using var handler = new HttpClientHandler()
     {
         CheckCertificateRevocationList = true
     };
+
     using var client = new HttpClient(handler);
     client.BaseAddress = new Uri("http://localhost:8080");
 
-    Console.WriteLine("Checking health ...");
-
-    var healthResponse = await client.GetAsync(new Uri("/health", UriKind.Relative));
-
-    if (!healthResponse.IsSuccessStatusCode)
+    // executes a GET request and returns the response if successful
+    var ping = async (string endpoint) =>
     {
-        Exit($"Health endpoint returned {healthResponse.StatusCode}: verify that api is running.");
-    }
+        var response = await client.GetAsync(new Uri(endpoint, UriKind.Relative));
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Exit($"Error: {endpoint} endpoint returned {response.StatusCode}.");
+        }
+
+        return response;
+    };
+
+    // executes a POST request and returns the corresponding CarResponse DTO if successful
+    var pingCar = async (string endpoint, HttpMethod method) =>
+    {
+        HttpResponseMessage response;
+
+        if (method == HttpMethod.Get)
+        {
+            response = await client.GetAsync(new Uri(endpoint, UriKind.Relative));
+        }
+        else if (method == HttpMethod.Post)
+        {
+            response = await client.PostAsync(new Uri(endpoint, UriKind.Relative), null);
+        }
+        else
+        {
+            throw new NotSupportedException($"Method {method} not supported");
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Exit($"Error: {endpoint} endpoint returned {response.StatusCode}.");
+        }
+
+        var car = await response.Content.ReadFromJsonAsync<CarResponse>();
+        Console.WriteLine($"Returned {JsonSerializer.Serialize(car)}");
+        return car;
+    };
+
+    var moveCar = async (CarResponse carToMove) =>
+    {
+        
+        Console.WriteLine($"Moving car #{carToMove.Id} to next stop ...");
+
+        var response = await client.PostAsync(new Uri($"/cars/{carToMove.Id}/move", UriKind.Relative), null);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Exit($"Error: /cars/move endpoint returned {response.StatusCode}.");
+        }
+
+        var movedCar = await response.Content.ReadFromJsonAsync<CarResponse>();
+        Console.WriteLine($"Returned {JsonSerializer.Serialize(movedCar)}");
+        Console.WriteLine("");
+        
+        return movedCar;
+    };
+
+    #endregion
+
+    Console.WriteLine("Checking health ...");
+    await ping("/health");
+    Console.WriteLine();
+
+    Console.WriteLine("Requesting car for floor #1 ...");
+    var lobby = await pingCar("/cars/call/1", HttpMethod.Post);
+    Console.WriteLine();
+
+    await moveCar(lobby);
+
+    Console.WriteLine($"Adding stop to car #{lobby.Id} ...");
+    await pingCar($"/cars/{lobby.Id}/stops/2", HttpMethod.Post);
+    Console.WriteLine();
+
+    Console.WriteLine($"Adding stop to car #{lobby.Id} ...");
+    await pingCar($"/cars/{lobby.Id}/stops/5", HttpMethod.Post);
+    Console.WriteLine();
+
+    await moveCar(lobby);
+
+    Console.WriteLine("Requesting car for floor #-2 ...");
+    var basement = await pingCar("/cars/call/-2", HttpMethod.Post);
+    Console.WriteLine();
+
+    Console.WriteLine("Requesting car for floor #5 ...");
+    var fifthFloor = await pingCar("/cars/call/5", HttpMethod.Post);
+    Console.WriteLine();
+
+    await moveCar(basement);
+
+    Console.WriteLine("All done.  Press any key to exit...");
+    Console.ReadKey();
 }
 catch (HttpRequestException ex)
 {
